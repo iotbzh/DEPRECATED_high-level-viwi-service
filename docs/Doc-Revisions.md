1cd92a82c9efb72785903cf2fe089f1c3e9ef6dd
Stephane Desneux <stephane.desneux@iot.bzh>
2017-09-20 17:44:56 +0200

    doc: bump revision to 4.99-EERC1
    Change-Id: I0b8f1d4594216a23ee252044daa93320b9f515a9
    Signed-off-by: Stephane Desneux <stephane.desneux@iot.bzh>

--- docs/Doc-Revisions.md
+++ docs/Doc-Revisions.md
@@ -4,4 +4,5 @@ Document revisions
 | Date        | Version | Designation                          | Author                  |
 |-------------|---------|--------------------------------------|-------------------------|
 | 07 Jun 2017 |   1.0   | Initial release                      | R.Forlot [ Iot.bzh ]    |
-| 20 Jun 2017 |   4.0   | Initial release                      | R.Forlot [ Iot.bzh ]    |
+| 20 Jun 2017 |   4.0   | Update for AGL DD                    | R.Forlot [ Iot.bzh ]    |
+| 20 Sep 2017 |   4.99-EERC1   | Update for AGL EE.RC1         | R.Forlot [ Iot.bzh ]    |
--- docs/resources/make_cover.sh
+++ docs/resources/make_cover.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+DOCS_DIR=$(cd $(dirname $0)/.. && pwd)
+BOOKFILE=$DOCS_DIR/../book.json
 
-cat cover.svg | sed -e 's/{title}/High Level Viwi CAN binding/' \
-    -e 's/font-size:87.5px/font-size:50px/g' \
-    -e 's/{subtitle}//g' \
-    -e 's/{version}/Version 4.0/g' \
-    -e 's/{date}/June 2017/g' \
+TITLE=$(grep  '"title":' $BOOKFILE | cut -d'"' -f 4)
+SUBTITLE=$(grep  '"subtitle":' $BOOKFILE | cut -d'"' -f 4)
+VERSION="Version $(grep  '"version":' $BOOKFILE | cut -d'"' -f 4)"
+DATE=$(grep  '"published":' $BOOKFILE | cut -d'"' -f 4)
+
+[ -z "$TITLE" ] && { echo "Error TITLE not set!" ; exit 1; }
+[ -z "$VERSION" ] && { echo "Error VERSION not set!" ; exit 1; }
+[ -z "$DATE" ] && { echo "Error DATE not set!" ; exit 1; }
+
+
+cat $(dirname $0)/cover.svg | sed -e "s/{title}/$TITLE/g" \
+    -e "s/font-size:87.5px/font-size:54px/g" \
+    -e "s/{subtitle}/$SUBTITLE/g" \
+    -e "s/font-size:62.5px/font-size:40px/g" \
+    -e "s/{version}/$VERSION/g" \
+    -e "s/{date}/$DATE/g" \
     > /tmp/cover.svg
 
 # use  imagemagick convert tool  (cover size must be 1800x2360)
 convert -resize "1600x2160!" -border 100 -bordercolor white -background white \
-    -flatten -quality 100 /tmp/cover.svg ../cover.jpg
+    -flatten -quality 100 /tmp/cover.svg $DOCS_DIR/cover.jpg
 
-convert -resize "200x262!" ../cover.jpg ../cover_small.jpg
+convert -resize "200x262!" $DOCS_DIR/cover.jpg $DOCS_DIR/cover_small.jpg
