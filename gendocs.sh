4f1c7e68e5c73f6dcb54b6495b9e3de8456f0376
Romain Forlot <romain.forlot@iot.bzh>
2017-12-21 17:49:35 +0100

    Update gitbook templates
    Updating cover automatically using metadata in books.json
    
    Change-Id: I898fa57c5c86c671aec55f31db0553d969326b26
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- gendocs.sh
+++ gendocs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# shellcheck disable=SC2086
 
-SCRIPT=$(basename $BASH_SOURCE)
+OUTFILENAME="High_Level_ViWi_Service"
+
+SCRIPT=$(basename "${BASH_SOURCE[@]}")
+
+VERSION=$(grep '"version":' "$(dirname "${BASH_SOURCE[@]}")/book.json" | cut -d'"' -f 4)
+[ "$VERSION" != "" ] && OUTFILENAME="${OUTFILENAME}_v${VERSION}"
+
 
 function usage() {
 	cat <<EOF >&2
@@ -30,38 +37,48 @@ DEBUG_FLAG=""
 DRY=""
 DO_ACTION=""
 OUT_DIR=./build
+ARGS=""
 
-[[ $? != 0 ]] && usage
+[ $# = 0 ] && usage
 while [ $# -gt 0 ]; do
 	case "$1" in
-		--debug) DEBUG_FLAG="--log=debug --debug";;
-		-d|--dry) DRY=echo;;
-		-h|--help) usage;;
-        pdf | serve | doxygen) DO_ACTION=$1;;
-		--) break;;
+		--debug)                DEBUG_FLAG="--log=debug --debug";;
+		-d|--dry)               DRY="echo";;
+		-h|--help)              usage;;
+        pdf | serve | doxygen)  DO_ACTION=$1;;
+		--)                     shift; ARGS="$ARGS $*"; break;;
+        *)                      ARGS="$ARGS $1";;
 	esac
     shift
 done
 
-cd $(dirname $0)
-ROOTDIR=`pwd -P`
+cd "$(dirname "$0")" || exit 1
+ROOTDIR=$(pwd -P)
 
 # Create out dir if needed
 [ -d $OUT_DIR ] || mkdir -p $OUT_DIR
 
-if [ "$DO_ACTION" = "pdf" -o "$DO_ACTION" = "serve" ]; then
-    GITBOOK=`which gitbook`
+if [ "$DO_ACTION" = "pdf" ] || [ "$DO_ACTION" = "serve" ]; then
+
+    GITBOOK=$(which gitbook)
     [ "$?" = "1" ] && { echo "You must install gitbook first, using: sudo npm install -g gitbook-cli"; exit 1; }
 
-    EBCONV=`which ebook-convert`
+    which ebook-convert > /dev/null 2>&1
     [ "$?" = "1" ] && { echo "You must install calibre first, using: 'sudo apt install calibre' or refer to https://calibre-ebook.com/download"; exit 1; }
 
+    [ ! -d "$ROOTDIR/node_modules" ] && $GITBOOK install
+
     if [ "$DO_ACTION" = "pdf" ]; then
-	    OUTFILE=$OUT_DIR/HighLevelCanBinding_Guide.pdf
-        $DRY $GITBOOK pdf $ROOTDIR $OUTFILE $DEBUG_FLAG
-        [ "$?" = "0" ] && echo "PDF has been successfully generated in $OUTFILE"
+
+        # Update cover when book.json has been changed
+        [[ $ROOTDIR/book.json -nt $ROOTDIR/docs/cover.jpg ]] && { echo "Update cover files"; $ROOTDIR/docs/resources/make_cover.sh || exit 1; }
+
+	    OUTFILE=$OUT_DIR/$OUTFILENAME.pdf
+        if $DRY $GITBOOK pdf $ROOTDIR $OUTFILE $DEBUG_FLAG $ARGS; then
+            echo "PDF has been successfully generated in $OUTFILE"
+        fi
     else
-        $DRY $GITBOOK serve $DEBUG_FLAG
+        $DRY $GITBOOK serve $DEBUG_FLAG $ARGS
     fi
 
 elif [ "$DO_ACTION" = "doxygen" ]; then
